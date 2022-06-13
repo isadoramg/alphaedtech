232ce951caedbebd0c2ef820e04341dde4f10df0
isadoramg <isadoramguerra@gmail.com>
2022-06-13 03:38:33 -0300

    update

---
+++ javascript/aula03/index.js
@@ -0,0 +1,19 @@
+function calc() {
+    const operando1 = parseInt(document.querySelector("#operando1").value, 10);
+    const operando2 = parseInt(document.querySelector("#operando2").value, 10);
+    const operador = document.querySelector("operador").value;
+
+    let resultado;
+
+    if (operador === "soma") {
+        resultado = operando1 + operando2;
+    } else if (operador === "subtração") {
+        resultado = operando1 - operando2;
+    } else if (operador === "multiplicação") {
+        resultado = operando1 * operando2;
+    } else if (operador === "divisão") {
+        resultado = operando1 / operando2;
+    };
+
+    document.querySelector("#resultado").textContent = "Resultado: " + resultado;
+}
